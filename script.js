398dd3801ac97f6c6025ea8e15c85b9aef57c9fc
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2023-06-03 19:24:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If required
+        let button = document.getElementsByTagName('input')[0];
+        button.addEventListener('click',function(){
+          let select = document.getElementById("colorSelect");
+          const selectedIndex = colorSelect.selectedIndex;
+          
+         select.remove(selectedIndex);
+        });
+
